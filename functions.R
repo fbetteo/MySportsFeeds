a964e1ee4dcc17267b6898825389109b59a53a3b
Franco <francobetteo@gmail.com>
2019-02-03 12:43:15 -0300

    Adding Daily Team Gamelogs

--- functions.R
+++ functions.R
@@ -9,16 +9,31 @@
 # formato = formato de la descarga ("csv", "json", "xml")
 # output = nombre del archivo rds a exportar a data/raw
  
-API_request <- function(version = "2.0", league = "nba", season, feed, formato, output){
+API_request <- function(version = "2.0", league = "nba", season, feed, formato, output, ...){
   
     library(mysportsfeedsR)
     authenticate_v2_x('4eec6849-46c9-43d2-9280-711e0c')
   
       output_file <- msf_get_results( version = version, league = league, season = season, feed = feed,
-                                  params = list(format = formato))
+                                  params = list(format = formato, ...))
       
       saveRDS(object = output_file, file = here::here("data","raw", paste0(output,".rds")))
 }
 
 
+# Version para daily_team_gamelogs. No se como inputar la ruta de export en el here::here si lo paso como parametro.
+# Solo cambia el parametro "file" de saveRDS.
+
+API_request_daily_team_gamelogs <- function(version = "2.0", league = "nba", season, feed, formato, output, ...){
+  
+  library(mysportsfeedsR)
+  authenticate_v2_x('4eec6849-46c9-43d2-9280-711e0c')
+  
+  output_file <- msf_get_results( version = version, league = league, season = season, feed = feed,
+                                  params = list(format = formato, ...))
+  
+  saveRDS(object = output_file, file = here::here("data","raw","daily_team_gamelogs", paste0(output,".rds")))
+  
+  
+}
 
