196e3ff74b64069551bd5eeb566d8ad6a821eaaa
Franco <francobetteo@gmail.com>
2019-02-26 00:11:22 -0300

    Lineup + play by play

--- functions.R
+++ functions.R
@@ -37,3 +37,38 @@ API_request_daily_team_gamelogs <- function(version = "2.0", league = "nba", sea
   
 }
 
+
+# Version para play by play. No se como inputar la ruta de export en el here::here si lo paso como parametro.
+# Solo cambia el parametro "file" de saveRDS.
+
+
+API_request_play_by_play <- function(version = "2.0", league = "nba", season, feed, formato, output, ...){
+  
+  library(mysportsfeedsR)
+  authenticate_v2_x('4eec6849-46c9-43d2-9280-711e0c')
+  
+  output_file <- msf_get_results( version = version, league = league, season = season, feed = feed,
+                                  params = list(format = formato, ...))
+  
+  saveRDS(object = output_file, file = here::here("data","raw","play_by_play", paste0(output,".rds")))
+  
+  
+}
+
+
+# Version para lineu`p. No se como inputar la ruta de export en el here::here si lo paso como parametro.
+# Solo cambia el parametro "file" de saveRDS.
+
+
+API_request_lineup <- function(version = "2.0", league = "nba", season, feed, formato, output, ...){
+  
+  library(mysportsfeedsR)
+  authenticate_v2_x('4eec6849-46c9-43d2-9280-711e0c')
+  
+  output_file <- msf_get_results( version = version, league = league, season = season, feed = feed,
+                                  params = list(format = formato, ...))
+  
+  saveRDS(object = output_file, file = here::here("data","raw","lineup", paste0(output,".rds")))
+  
+  
+}
---
+++ process_stints_WIP.R
@@ -0,0 +1,60 @@
+# Prueba play by play
+
+library(tidyverse)
+raw <- readRDS(here::here("data","raw","play_by_play","play_by_play20171017-BOS-CLE.RDS"))
+raw_plays <- raw$api_json$plays
+
+subs <- raw_plays %>% select(description, playStatus.quarter, playStatus.secondsElapsed, starts_with("substitution")) %>%
+  filter(substitution.team.id != "NA")
+
+head(raw_plays)
+
+glimpse(subs)
+
+aa <- subs %>% select(playStatus.quarter, playStatus.secondsElapsed, substitution.team.id, substitution.incomingPlayer.id, substitution.outgoingPlayer.id)
+
+head(aa)
+
+a2 <- aa %>% 
+  group_by(playStatus.quarter, playStatus.secondsElapsed) %>%
+  nest()
+
+a2 <- a2 %>% mutate(stint = row_number())
+a2
+
+n_stints <- a2 %>% summarise( count = max(stint))
+n_stints
+lineup <- readRDS(here::here("data","raw","lineup","lineup20171017-BOS-CLE.RDS"))
+
+raw_lineup <- lineup$api_json$teamLineups
+raw_lineup[4]
+str(raw_lineup)
+
+bosline <- raw_lineup$actual.lineupPositions[[1]]
+bosline
+
+str(ex1)
+ex1 <- a2[2,]
+ex1$data[[1]]
+
+bosupd <- bosline %>% left_join(ex1$data[[1]], by = c("player.id" = "substitution.outgoingPlayer.id"))
+
+bosupd
+match_lineup <- vector("list", length = (n_stints[
+[1]]+1))
+
+a2[1,]$data[[1]]
+
+match_lineup[[1]] <- bosline
+for (i in 2:n_stints[[1]]){
+  print(i)
+  match_lineup[[i]] <- match_lineup[[i-1]] %>% left_join(a2[(i-1),]$data[[1]], by = c("player.id" = "substitution.outgoingPlayer.id"))
+  match_lineup[[i]] <- match_lineup[[i]] %>% mutate(player.id = ifelse(is.na(substitution.incomingPlayer.id), player.id, substitution.incomingPlayer.id)) %>%
+    select(-substitution.incomingPlayer.id)
+}
+
+match_lineup[[1]]
+match_lineup[[2]]
+match_lineup[[3]]
+
+match_lineup[[1]] == match_lineup[[2]]
